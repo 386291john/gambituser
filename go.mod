66958cd1ebc37550031a512cff9aa8c24d8c4a6d
386291john <srcaballero144@gmail.com>
2023-09-24 23:39:02 -0500

    configuracion lambda

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/386291john/gambituser
+
+go 1.21.0
+
+require github.com/aws/aws-lambda-go v1.41.0
--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
-import (
-	"context"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-)
-
-func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// Tu lógica aquí
-}
+import lambda "github.com/aws/aws-lambda-go/lambda"
 
 func main() {
 	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda() {
+
 }
