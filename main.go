7031e50e4610c759e34e33abc47538b888ce4d2a
386291john <srcaballero144@gmail.com>
2023-09-25 19:46:48 -0500

    configuracion creacion de main

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import lambda "github.com/aws/aws-lambda-go/lambda"
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	lambda "github.com/aws/aws-lambda-go/lambda"
+)
 
 func main() {
 	lambda.Start(EjecutoLambda)
 }
 
-func EjecutoLambda() {
+func EjecutoLambda(ctx context.Context, event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error) {
 
 }
